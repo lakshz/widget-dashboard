06192bcc6a7d7512525b4c19be88d75c9f847335
Lakshya Satpal <lakshyasatpal372@gmail.com>
2024-02-23 13:45:55 +0530

    reusable data widget

--- src/components/widgets/Widgets.tsx
+++ src/components/widgets/Widgets.tsx
@@ -5,13 +5,17 @@ import widgetsData from "./widgets-data.json";
 const Widgets = () => {
   const widgets = widgetsData.widgets;
   return (
-    <div className="h-[90vh] bg-[#f4f4ff] px-10 py-6">
-      <DataWidget
-        type={widgets[0].dataType!}
-        bgColor={widgets[0].bgColor as WidgetBgColors}
-        size={widgets[0].size as [number, number]}
-        data={widgets[0].data}
-      />
+    <div className="min-h-[90vh] grid grid-cols-6 grid-rows-4 gap-8 bg-[rgb(244,244,255)] px-10 py-6">
+      {widgets.map((widget) => (
+        <DataWidget
+          key={widget.id}
+          type={widget.dataType!}
+          bgColor={widget.bgColor as WidgetBgColors}
+          start={widget.start as [number, number]}
+          end={widget.end as [number, number]}
+          data={widget.data}
+        />
+      ))}
     </div>
   );
 };
--- src/components/widgets/cards/DataWidget.tsx
+++ src/components/widgets/cards/DataWidget.tsx
@@ -15,7 +15,8 @@ export enum WidgetBgColors {
 type DataWidgetProps = {
   type: DataWidgetTypes;
   bgColor: WidgetBgColors;
-  size: [number, number];
+  start: [number, number];
+  end: [number, number];
   data: {
     headingRow: string[];
     values: string[][];
@@ -26,21 +27,24 @@ type DataWidgetProps = {
 // based on the bg color, we map all the colors for widget text
 const ColorsMapping = {
   [WidgetBgColors.WHITE]: {
-    topNav: "text-primary",
+    topNav1: "text-primary border-b-neutral-200",
+    topNav2: "text-neutral-500",
     headingRow1: "text-primary",
     headingRow2: "text-[#9f9cfa]",
     values: "text-neutral-500",
     totalRow: "text-neutral-800",
   },
   [WidgetBgColors.PRIMARY]: {
-    topNav: "text-white",
+    topNav1: "text-white  border-b-white",
+    topNav2: "text-neutral-100",
     headingRow1: "text-white font-medium",
     headingRow2: "text-neutral-200",
     values: "text-neutral-300",
     totalRow: "text-neutral-100",
   },
   [WidgetBgColors.DARK]: {
-    topNav: "text-white",
+    topNav1: "text-white  border-b-white",
+    topNav2: "text-neutral-200",
     headingRow1: "text-white font-medium",
     headingRow2: "text-neutral-200",
     values: "text-neutral-200",
@@ -48,35 +52,56 @@ const ColorsMapping = {
   },
 };
 
-const DataWidget = ({ bgColor, size, type, data }: DataWidgetProps) => {
+const DataWidget = ({ bgColor, start, end, type, data }: DataWidgetProps) => {
   return (
     <div
-      className={clsx("shadow-lg rounded-[20px] p-4 w-[300px]", {
-        "bg-white": bgColor === WidgetBgColors.WHITE,
-        "bg-primary": bgColor === WidgetBgColors.PRIMARY,
-        "bg-neutral-800": bgColor === WidgetBgColors.DARK,
-      })}
+      className={clsx(
+        `shadow-lg rounded-[20px] p-4 row-start-${start[0]} col-start-${start[1]} col-end-${end[1]} row-end-${end[0]}`,
+        {
+          "bg-white": bgColor === WidgetBgColors.WHITE,
+          "bg-primary": bgColor === WidgetBgColors.PRIMARY,
+          "bg-neutral-800": bgColor === WidgetBgColors.DARK,
+        }
+      )}
     >
-      <div className="border-b border-b-neutral-200 pb-1 flex w-full justify-between">
+      <div className="border-b pb-1 flex w-full justify-between">
         {type === DataWidgetTypes.HISTORIC ? (
           <div className="flex w-[60%] text-center">
             <div
               className={clsx(
                 "basis-1/3 -mb-1 border-b-2 font-medium border-b-primary",
                 {
-                  [ColorsMapping[bgColor].topNav]: true,
+                  [ColorsMapping[bgColor].topNav1]: true,
                 }
               )}
             >
               7d
             </div>
-            <div className="cursor-pointer basis-1/3 text-neutral-500">14d</div>
-            <div className="cursor-pointer basis-1/3 text-neutral-500">30d</div>
+            <div
+              className={clsx("cursor-pointer basis-1/3", {
+                [ColorsMapping[bgColor].topNav2]: true,
+              })}
+            >
+              14d
+            </div>
+            <div
+              className={clsx("cursor-pointer basis-1/3", {
+                [ColorsMapping[bgColor].topNav2]: true,
+              })}
+            >
+              30d
+            </div>
           </div>
         ) : (
-          <div className="text-neutral-500">Today</div>
+          <div className={clsx({ [ColorsMapping[bgColor].topNav2]: true })}>
+            Today
+          </div>
         )}
-        <div className="text-neutral-400 cursor-pointer">
+        <div
+          className={clsx("cursor-pointer", {
+            [ColorsMapping[bgColor].topNav2]: true,
+          })}
+        >
           <MoreHorizontal />
         </div>
       </div>
@@ -103,6 +128,7 @@ const DataWidget = ({ bgColor, size, type, data }: DataWidgetProps) => {
           <tbody>
             {data.values.map((row, i) => (
               <tr
+                key={i}
                 className={clsx("grid grid-cols-3 gap-1 text-center", {
                   [ColorsMapping[bgColor].values]: true,
                 })}
